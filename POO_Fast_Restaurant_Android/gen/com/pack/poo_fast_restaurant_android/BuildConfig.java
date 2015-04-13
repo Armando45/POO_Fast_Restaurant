c86a37c970c9cdc21c05cad6e2ca11010b945848
Armando <armando55026@gmail.com>
2015-04-13 14:46:47 -0300

    .

---
+++ POO_Fast_Restaurant_Android/gen/com/pack/poo_fast_restaurant_android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.pack.poo_fast_restaurant_android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
